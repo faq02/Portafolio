253707b722637351f239f73636b121d8ef574dc0
faq02 <facuuromo216@gmail.com>
2022-04-20 01:56:04 -0300

    Se actualizo el master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { RedesComponent } from './component/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule
