78a6d61100d161b494c03670bd085e93ea175aa1
faq02 <facuuromo216@gmail.com>
2022-04-24 14:05:45 -0300

    Se agrego boton de Login  y se creo su componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { RedesComponent } from './component/redes/redes.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RedesComponent
+    RedesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
